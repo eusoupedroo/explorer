85e61045f28461b9d2495d16226a799818dcdd05
Pedro Sousa <jpedroleitedesousa@gmail.com>
2023-03-14 15:43:37 -0300

    Website Initial Page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Explorer</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+		<div class="mainSection">
+
+			<div class="logoContainer">
+				<img src="assets/images/logo/logo.png">
+			</div>
+
+
+			<div class="searchContainer">
+				<form action="search.php" method="GET">
+					<input class="searchBox" type="text" name="term">
+					<input class="searchButton" type="submit" value="buscar">
+					
+				</form>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html> 
