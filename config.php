5bd825ed002ee148e013bba6c8f17b0060505c48
Pedro Sousa <jpedroleitedesousa@gmail.com>
2023-03-14 15:44:53 -0300

    Connection With Database Of The Project

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+ob_start();
+
+try {
+
+    
+    $con = new PDO("mysql:dbname=searchcrawler; host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+
+} catch(PDOException $e) {
+    echo "Connection Failed ". $e->getMessage();
+}
+
+?> 
