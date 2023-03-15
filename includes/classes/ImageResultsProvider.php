e7991736e893249e299ba2433c5e3b294a02fd83
Pedro Sousa <jpedroleitedesousa@gmail.com>
2023-03-15 15:04:41 -0300

    Image Track Class

--- includes/classes/ImageResultsProvider.php
+++ includes/classes/ImageResultsProvider.php
@@ -68,7 +68,7 @@ class ImageResultsProvider {
 			}
 			
 			$resultsHtml .= "<div class='gridItem image$count'>
-								<a href='$imageUrl'>
+								<a href='$imageUrl' data-fancybox data-caption='$displayText'>
 									<script>
 										$(document).ready(function () {
 											loadImage(\"$imageUrl\", \"image$count\");
